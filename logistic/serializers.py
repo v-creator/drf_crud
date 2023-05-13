5d589426aaa9585beaab23e4e3e7b1356f01e31b
17845001 <Kolesnikov.V.Aleks@sberbank.ru>
2023-05-13 16:02:54 +0300

    save change

---
+++ logistic/serializers.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+
+from logistic.models import Product, Stock, StockProduct
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'products', 'positions']
+
+    def create(self, validated_data):
+        positions = validated_data.pop('positions')
+
+        stock = super().create(validated_data)
+
+        for position in positions:
+            StockProduct.objects.create(
+                stock=stock,
+                product=position['product'],
+                quantity=position['quantity'],
+                price=position['price']
+            )
+
+        return stock
+
+    def update(self, instance, validated_data):
+        positions = validated_data.pop('positions')
+
+        stock = super().update(instance, validated_data)
+
+        for position in positions:
+            StockProduct.objects.filter(stock=stock, product=position['product'].id).update(
+                quantity=position['quantity'],
+                price=position['price']
+            )
+
+        return stock
